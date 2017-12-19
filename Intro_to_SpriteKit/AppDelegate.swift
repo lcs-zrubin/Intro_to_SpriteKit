e2a47ddf1f7013c5e60a16ae0a44ead91c2f3b8d
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-19 12:41:29 -0500

    Initial Commit

---
+++ Intro_to_SpriteKit/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Intro_to_SpriteKit
+//
+//  Created by Gordon, Russell on 2017-12-19.
+//  Copyright © 2017 Gordon, Russell. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ Intro_to_SpriteKit/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Intro_to_SpriteKit
+//
+//  Created by Gordon, Russell on 2017-12-19.
+//  Copyright © 2017 Gordon, Russell. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
