97a0d6423c12fd3d643c88fd1e99ac01a844597b
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-19 13:43:51 -0500

    Intro and developer documentation pages

---
+++ SpriteKit_Fundamental_Concepts.playground/Pages/Developer Documentation.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+/*:
+ # Developer Documentation
+ 
+ You can find very detailed developer documentation included with Xcode.
+ 
+ Open the **Help** menu, then choose **Developer Documentation**.
+ 
+ ![help-menu](help-menu.png "Help menu")
+ 
+ Scroll down on the left hand side of the window until you see SpriteKit, then select that option.
+
+ ![spritekit](spritekit.png "SpriteKit documentation")
+ 
+ Use the developer documentation to dive deep into what you can do with SpriteKit.
+ 
+ You will get an introduction to the very basics of SpriteKit on the following pages.
+ 
+ */
+//: [Next](@next)
+
+
+
---
+++ SpriteKit_Fundamental_Concepts.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+/*:
+ # Introduction
+ 
+ The purpose of this playground is to provide you with a very brief introduction to the fundamental concepts behind SpriteKit.
+ 
+ SpriteKit is Apple's framework to allow for the development of 2D games.
+ 
+ */
+//: [Next](@next)
+
+
